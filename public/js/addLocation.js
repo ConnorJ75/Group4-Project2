27eb5358eb27ea2350b4d1aa3ca7c36711ee084e
Michael Salerno <mike.salano@gmail.com>
2024-02-12 18:09:07 -0500

    Updated location adding page

---
+++ public/js/addLocation.js
@@ -0,0 +1,49 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#location-name').value.trim();
+    const locationPhone = document.querySelector('#location-phone').value.trim();
+    const locationAddress = document.querySelector('#location-address').value.trim();
+    const locationURL = document.querySelector('#location-url').value.trim();
+  
+    if (name && needed_funding && description) {
+      const response = await fetch(`/api/locations`, {
+        method: 'POST',
+        body: JSON.stringify({ name, locationPhone, locationAddress, locationURL }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/addLocation');
+      } else {
+        alert('Failed to create project');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/locations/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/addLocation');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-location-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.location-list')
+    .addEventListener('click', delButtonHandler);
+  
