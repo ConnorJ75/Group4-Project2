ddacad3dd26fd51afc96d2148014281cb27c64d5
Michael Salerno <mike.salano@gmail.com>
2024-02-26 19:21:46 -0500

    actually fixed the issues in my last push

--- public/js/addLocation.js
+++ public/js/addLocation.js
@@ -23,25 +23,7 @@ const newFormHandler = async (event) => {
   }
 };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/locations/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert('Failed to delete project');
-    }
-  }
-};
-
-document
-  .querySelector('.location-list')
-  .addEventListener('click', delButtonHandler);
 document
   .querySelector('.new-location-form')
   .addEventListener('submit', newFormHandler);
